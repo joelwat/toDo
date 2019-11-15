a759868f7821762af57d1a2c39343523bb5fcd11
Joel Watson <joelwat@gmail.com>
2019-11-15 12:39:10 -0800

    Adds toDo page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,67 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <div class="row">
+      <div class="col">
+        <q-form @submit="submit">
+          <q-input
+            outlined
+            v-model="itemText"
+          >
+            <template v-slot:after>
+              <q-btn
+                type="submit"
+                color="red"
+                label="Save"
+                style="display: inline-block;"
+              />
+            </template>
+          </q-input>
+        </q-form>
+      </div>
+
+      <div class="col">
+        <q-list
+          bordered
+          separator
+          v-if="items"
+        >
+          <q-item
+            clickable v-for="item in items"
+            :key="item"
+          >
+            <q-item-section>
+              {{ item }}
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'PageIndex',
+
+  data() {
+    return {
+      itemText: '',
+    };
+  },
+  computed: {
+    ...mapGetters('todo', [
+      'items',
+    ]),
+  },
+  methods: {
+    submit() {
+      this.$store.dispatch('todo/addItem', this.itemText);
+
+      // Clear item input for new item
+      this.itemText = '';
+    },
+  },
 };
 </script>
